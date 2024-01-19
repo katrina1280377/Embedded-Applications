84ee60a4dc58bb839a522e9132b10ab8d14feca1
Katrina Hsiung <147007495+katrina1280377@users.noreply.github.com>
2024-01-18 16:18:51 -0800

    revert

--- hw_2/solutions/solutions.h
+++
@@ -1,9 +0,0 @@
-#ifndef SOLUTIONS_H
-#define SOLUTIONS_H
-
-int running_total(int x);
-int* reverse(int* arr, int len);
-void reverse_in_place(int* arr, int len);
-int num_occurrences(int* arr, int len, int value);
-
-#endif
